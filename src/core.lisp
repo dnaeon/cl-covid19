3a4bddd157834b4a826b30785b9e88e1a718c7f7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-06-10 12:02:24 +0300

    core: add separate functions for syncing each database table

--- src/core.lisp
+++ src/core.lisp
@@ -12,11 +12,23 @@
    :get-countries-data
    :get-summary-data)
   (:export
-   :update-db-data))
+   :update-db-countries-data
+   :update-db-summary-data
+   :update-db-all-data))
 (in-package :cl-covid19.core)
 
-(defun update-db-data (api-client db-conn)
+(defun update-db-countries-data (api-client db-conn)
+  "Updates the local database with the latest countries data from the remote API"
+  (log:debug "Updating database with latest countries data from remote API")
+  (persist-countries-data (get-countries-data api-client) db-conn))
+
+(defun update-db-summary-data (api-client db-conn)
+  "Updates the local database with the latest summary data from the remote API"
+  (log:debug "Updating database with latest summary data from remote API")
+  (persist-summary-data (getf (get-summary-data api-client) :|Countries|) db-conn))
+
+(defun update-db-all-data (api-client db-conn)
   "Updates the local data with the data retrieved from the remote API"
   (log:debug "Updating database with latest data from remote API")
-  (persist-countries-data (get-countries-data api-client) db-conn)
-  (persist-summary-data (getf (get-summary-data api-client) :|Countries|) db-conn))
+  (update-db-countries-data api-client db-conn)
+  (update-db-summary-data api-client db-conn))
